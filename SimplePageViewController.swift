e3eea983e3ac8657ddeb991c83dacc57184cb25f
oliver.t.poole <oliverpoole7@gmail.com>
2016-01-03 13:53:23 +0000

    Added SimplePageViewController

---
+++ SimplePageViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SimplePageViewController.swift
+//  SmartDepart
+//
+//  Created by Oliver Poole on 03/01/2016.
+//  Copyright © 2016 OPApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SimplePageViewController : UIPageViewController {
+    
+    var customViewControllers : Array<UIViewController>
+    
+    /**
+     Initalises a new UIPageViewController and sets the view controllers passsed as a 
+     parameter to the datasource
+     
+     - parameter viewControllers: The view controllers included in the UIPageViewController
+     */
+    init (withViewControllers viewControllers : Array<UIViewController>) {
+
+        // Initalise the properties
+        self.customViewControllers = viewControllers
+        
+        // Call the super designated initaliser
+        super.init(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
+        
+        // Set the UIPageViewController view controllers and default settings
+        setViewControllers([customViewControllers.first!], direction: .Forward, animated: true, completion: nil)
+        dataSource = self
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.customViewControllers = Array<UIViewController>()
+        
+        super.init(coder: aDecoder)
+    }    
+}
+
+extension SimplePageViewController : UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let index = customViewControllers.indexOf(viewController)
+        
+        if index == customViewControllers.count - 1 {
+            return nil
+        }
+        else {
+            return customViewControllers[index! + 1]
+        }
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let index = customViewControllers.indexOf(viewController)
+        
+        if index == 0 {
+            return nil
+        }
+        else {
+            return customViewControllers[index! - 1]
+        }
+    }
+    
+}
+
+
